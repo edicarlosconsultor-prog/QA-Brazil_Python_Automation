e003c8278278e7b8e8fdeeaac0891558efe9266c
EdicarlosOliveira <edicarlosconsultor@gmail.com>
2026-05-12 16:47:47 -0300

    Projeto Sprint 7 - configuração inicial dos testes

---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-a1a9bd18-61ef-4080-aeb4-da7919464e91.containerhub.tripleten-services.com?lng=pt'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
